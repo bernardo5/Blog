4da675ab4377bf94b06b78080a528d085900c6e5
bernardo5 <bernardo5@users.noreply.github.com>
2018-05-04 00:00:22 +0100

    Initial Commit

---
+++ blog.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, make_response, redirect, abort
+from flask_bootstrap import Bootstrap
+
+app=Flask(__name__)
+bootstrap=Bootstrap(app)
+
+@app.route('/')
+def landingPage():
+	return render_template('index.html')
+
+@app.route('/aboutUs')
+def aboutUs():
+	return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
+@app.route('/richDadPoorDad')
+def richDadPoorDad():
+	bookImage='richdadpoordad.jpg'
+	return render_template('richDadPoorDad.html', bookImage=bookImage)
+
+@app.route('/ComoFazerAmigosEINfluenciarPessoas')
+def ComoFazerAmigosEINfluenciarPessoas():
+    bookImage='ComoFazerAmigosEINfluenciarPessoas.jpg'
+    return render_template('ComoFazerAmigosEINfluenciarPessoas.html', bookImage=bookImage)
+
+@app.route('/DOTCOM')
+def DOTCOM():
+    bookImage='DOTCOM.jpg'
+    return render_template('DOTCOM.html', bookImage=bookImage)
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
